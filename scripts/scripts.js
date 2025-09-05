5c1c59e622e08bf2a8c03f3a1736a84b6fccb0f7
forsaken-killer <theredrouge29@gmail.com>
2025-09-04 20:19:54 -0400

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,9 @@
+const chosen = document.getElementById("Selected")
+const btn = document.getElementById("Choose")
 
+const optns = ["Ki ki ki ma ma ma", "Mass Infection!", "Harp-gooners! Fire!"]
+
+btn.onclick = () => {
+    let choice = Math.floor(Math.random() * optns.length)
+    chosen.textContent = "Choice: " + optns[choice]
+}
